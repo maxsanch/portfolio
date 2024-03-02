75b2f079ed482a7020fcfeabef377f591ef5e123
maxsanch <maxence.sanchez-varas-leclercq@uha.fr>
2024-03-02 18:09:34 +0100

    modifications header

--- js/index.js
+++ js/index.js
@@ -6,3 +6,17 @@ for (let i in SAE) {
     jsp += "<p> <a href='pages/lesprojets.html?number=" + i + "' class='lienfooter'>" + i + "</a></p>"
 }
 document.querySelector('#link').innerHTML = jsp
+
+document.addEventListener("DOMContentLoaded", function() {
+    var header = document.querySelector("header");
+  
+    window.addEventListener("scroll", function() {
+      var scrollPosition = window.scrollY;
+  
+      if (scrollPosition > 50) {
+        header.classList.add("scroll"); 
+      } else {
+        header.classList.remove("scroll");
+      }
+    });
+});
