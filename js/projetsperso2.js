1e397fa291f639c90f532d6b57b1c4caf4e461c0
maxsanch <maxence.sanchez-varas-leclercq@uha.fr>
2024-03-07 18:53:08 +0100

    amélioration data

--- js/projetsperso2.js
+++ js/projetsperso2.js
@@ -1,27 +1,9 @@
-let jsp = ""
-
 // afficher les liens dans le footer vers les différentes pages
 
 for (let i in SAE) {
     jsp += "<p> <a href='lesprojets.html?number=" + i + "' class='lienfooter'>" + i + "</a></p>"
 }
 document.querySelector('#link').innerHTML = jsp
-
-// affichage des projets persos 
-
-let photo=""
-for(let o=1; o<=12; o++){
-    photo+='<img src="../img/photos/bien'+o+'.jpg" class="perso" alt="photo"></img>'
-}
-console.log(photo);
-document.querySelector("#photo").innerHTML=photo
-
-let D=""
-for(let r=1; r<=5; r++){
-    D+='<img src="../img/3D/'+r+'.PNG" class="perso" alt="photo"></img>'
-}
-console.log(D);
-document.querySelector("#eho").innerHTML=D
 
 document.addEventListener("DOMContentLoaded", function() {
     var header = document.querySelector("header");
